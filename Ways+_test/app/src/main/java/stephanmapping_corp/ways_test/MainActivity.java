559447b2e6eb3698ad253d546c0d22a6e4d34ff4
austrian-code-wizard <moritz.stephan@gmx.at>
2016-11-05 22:39:36 +0100

    route implementation

--- Ways+_test/app/src/main/java/stephanmapping_corp/ways_test/MainActivity.java
+++ Ways+_test/app/src/main/java/stephanmapping_corp/ways_test/MainActivity.java
@@ -2,15 +2,22 @@ package stephanmapping_corp.ways_test;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.util.SortedList;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationListener;
@@ -18,14 +25,31 @@ import com.mapbox.mapboxsdk.location.LocationServices;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.Constants;
+import com.mapbox.services.commons.ServicesException;
+import com.mapbox.services.commons.geojson.LineString;
+import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.directions.v5.DirectionsCriteria;
+import com.mapbox.services.directions.v5.MapboxDirections;
+import com.mapbox.services.directions.v5.models.DirectionsResponse;
+import com.mapbox.services.directions.v5.models.DirectionsRoute;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private MapView mapView;
     private MapboxMap map;
     //private FloatingActionButton floatingActionButton;
     private LocationServices locationServices;
     private static final int PERMISSIONS_LOCATION = 0;
+    private DirectionsRoute currentRoute;
+    private Position origin = Position.fromCoordinates(-3.588098, 37.176164);
+    private Position destination = Position.fromCoordinates(-3.601845, 37.184080);
 
 
 
@@ -122,6 +146,20 @@ public class MainActivity extends AppCompatActivity {
                         // listener is registered again and will adjust the camera once again.
                         map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location), 16));
                         locationServices.removeLocationListener(this);
+                        origin = Position.fromCoordinates(location.getLongitude(), location.getLatitude());
+                        map.addMarker(new MarkerOptions()
+                                .position(new LatLng(origin.getLatitude(), origin.getLongitude()))
+                                .title("Origin")
+                                .snippet("Alhambra"));
+                        map.addMarker(new MarkerOptions()
+                                .position(new LatLng(destination.getLatitude(), destination.getLongitude()))
+                                .title("Destination")
+                                .snippet("Plaza del Triunfo"));
+                        try {
+                            getRoute(origin, destination);
+                        } catch (ServicesException servicesException) {
+                            servicesException.printStackTrace();
+                        }
                     }
                 }
             });
@@ -132,7 +170,67 @@ public class MainActivity extends AppCompatActivity {
         // Enable or disable the location layer on the map
         map.setMyLocationEnabled(enabled);
     }
+    private void getRoute(Position origin, Position destination) throws ServicesException {
 
+        MapboxDirections client = new MapboxDirections.Builder()
+                .setOrigin(origin)
+                .setDestination(destination)
+                .setProfile(DirectionsCriteria.PROFILE_CYCLING)
+                .setAccessToken(MapboxAccountManager.getInstance().getAccessToken())
+                .build();
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                // You can get the generic HTTP info about the response
+                Log.d(TAG, "Response code: " + response.code());
+                if (response.body() == null) {
+                    Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                    return;
+                } else if (response.body().getRoutes().size() < 1) {
+                    Log.e(TAG, "No routes found");
+                    return;
+                }
+
+                // Print some info about the route
+                currentRoute = response.body().getRoutes().get(0);
+                Log.d(TAG, "Distance: " + currentRoute.getDistance());
+                Toast.makeText(
+                        MainActivity.this,
+                        "Route is " + currentRoute.getDistance() + " meters long.",
+                        Toast.LENGTH_SHORT).show();
+
+                // Draw the route on the map
+                drawRoute(currentRoute);
+            }
+
+
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                Log.e(TAG, "Error: " + throwable.getMessage());
+                Toast.makeText(MainActivity.this, "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void drawRoute(DirectionsRoute route) {
+        // Convert LineString coordinates into LatLng[]
+        LineString lineString = LineString.fromPolyline(route.getGeometry(), Constants.OSRM_PRECISION_V5);
+        List<Position> coordinates = lineString.getCoordinates();
+        LatLng[] points = new LatLng[coordinates.size()];
+        for (int i = 0; i < coordinates.size(); i++) {
+            points[i] = new LatLng(
+                    coordinates.get(i).getLatitude(),
+                    coordinates.get(i).getLongitude());
+        }
+
+        // Draw Points on MapView
+        map.addPolyline(new PolylineOptions()
+                .add(points)
+                .color(Color.parseColor("#009688"))
+                .width(5));
+    }
     @Override
     public void onRequestPermissionsResult(
             int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
